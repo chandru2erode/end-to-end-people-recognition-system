fb7350346e91818e0af2142e89d893f935dd42b4
chandru2erode <chandrakumar.ec15@bitsathy.ac.in>
2020-03-16 02:15:14 +0530

    Video feed error fixed in face-recognition code

--- face-recognition.py
+++ face-recognition.py
@@ -37,39 +37,42 @@ def encode_known():
 def detect_nameless(known_face_encodings, known_face_names):
     # Select the webcam of the computer (0 by default for laptop)
     video_capture = cv2.VideoCapture(0)
-
+    # Initialize some variables
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
     # Apply it until you stop the file's execution
     while True:
         # Take every frame
-        frame = video_capture.read()
+        ret, frame = video_capture.read()
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
         # Process every frame only one time
-        if frame:
-            face_locations = []
+        if process_this_frame:
             # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(frame)
-            face_encodings = face_recognition.face_encodings(frame, face_locations)
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
             # Initialize an array for the name of the detected users
             face_names = []
             # * ---------- Initialize JSON to EXPORT --------- *
             json_to_export = {}
-            print(face_encodings)
             # Loop in every faces detected
             for face_encoding in face_encodings:
-                print(4)
                 # See if the face is a match for the known face(s)
                 matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
                 name = "Unknown"
-                print(5)
                 # check the known face with the smallest distance to the new face
                 face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
                 # Take the best one
                 best_match_index = np.argmin(face_distances)
                 # If we have a match
                 if matches[best_match_index]:
-                    print(6)
                     # Save the name of the best match
                     name = known_face_names[best_match_index]
-
+                    '''
                     # * ---------- SAVE data to send to the API -------- *
                     # Save the name
                     json_to_export['name'] = name
@@ -85,24 +88,29 @@ def detect_nameless(known_face_encodings, known_face_names):
                     # Make a POST request to the API
                     r = requests.post(url='http://127.0.0.1:5000/receive_data', json=json_to_export)
                     # Print to status of the request:
-                    print("Status: ", r.status_code)
-            print(7)
-            # Store the name in an array to display it later
-            face_names.append(name)
-            # To be sure that we process every frame only one time
-            process_this_frame = not process_this_frame
+                    print("Status: ", r.status_code) '''
+                # Store the name in an array to display it later
+                face_names.append(name)
+        # To be sure that we process every frame only one time
+        process_this_frame = not process_this_frame
 
-            # * --------- Display the results ---------- *
-            for (top, right, bottom, left), name in zip(face_locations, face_names):
-                # Draw a box around the face
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-                # Define the font of the name
-                font = cv2.FONT_HERSHEY_DUPLEX
-                # Display the name
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        # * --------- Display the results ---------- *
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            # Define the font of the name
+            font = cv2.FONT_HERSHEY_DUPLEX
+            # Display the name
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-            # Display the resulting image
-            cv2.imshow('Video', frame)
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     # Release handle to the webcam
     video_capture.release()
