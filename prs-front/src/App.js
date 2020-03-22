0f1325534fcccd87e3427cc996ae475ac0311821
Saroopashree <saroopa25@gmail.com>
2020-03-22 15:19:45 +0530

    React can now request the python backend to run facial_recognition.py script

--- prs-front/src/App.js
+++ prs-front/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import EmployeeDetails from "./components/empDetails";
 import AddEmployee from "./components/addEmp";
@@ -6,16 +6,46 @@ import ALlEmployeeDetails from "./components/allEmpDetails";
 import RecentRecords from "./components/recentRecords";
 import DeleteEmployee from "./components/deleteEmp";
 
-function App() {
-  return (
-    <div className="App">
-      {/* <EmployeeDetails /> */}
-      {/* <AddEmployee /> */}
-      {/* <ALlEmployeeDetails /> */}
-      {/* <RecentRecords /> */}
-      <DeleteEmployee />
-    </div>
-  );
+import axios from "axios";
+// import { PythonShell } from "python-shell";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: ""
+    };
+    this.runPythonScript();
+  }
+
+  runPythonScript = () => {
+    axios.get("http://127.0.0.1:5000/run_script").then(result => {
+      console.log(result);
+      this.setState({ response: result["data"]["message"] });
+    });
+
+    /* PythonShell.run(
+      "./backend-scripts/facial_recognition.py",
+      { mode: "text" },
+      function(err) {
+        if (err) throw err;
+        console.log("finished");
+      }
+    ); */
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.response}
+        {/* <EmployeeDetails /> */}
+        {/* <AddEmployee /> */}
+        {/* <ALlEmployeeDetails /> */}
+        {/* <RecentRecords /> */}
+        <DeleteEmployee />
+      </div>
+    );
+  }
 }
 
 export default App;
